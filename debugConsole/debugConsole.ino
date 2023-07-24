21eded475d02dff95e74678ae2fac54cda21eb36
Andrey Nosov <a.nosoff00@gmail.com>
2023-07-24 20:52:21 +0200

    Buffer overflow protection

--- debugConsole/debugConsole.ino
+++ debugConsole/debugConsole.ino
@@ -2,7 +2,9 @@
 
 void setup() {
   // initialize debug console
-  debugConsole.init(DEBUG_CONSOLE_MODE_SPI);
+  debugConsole.init(DEBUG_CONSOLE_MODE_UART_AND_SPI);
+  //debugConsole.init(DEBUG_CONSOLE_MODE_UART);
+  //debugConsole.init(DEBUG_CONSOLE_MODE_SPI);
 }
 
 void loop() {
@@ -12,11 +14,13 @@ void loop() {
   debugConsole.println(34.35);
   debugConsole.println(-34.35);
   debugConsole.println(0.123456789);
+  Serial.println(0.123456789);
   debugConsole.println(0.0012);
   debugConsole.print(-0.0012);
   debugConsole.println("");
   debugConsole.println(-0.0012);
   debugConsole.println(16777216.0625);
   debugConsole.println(23);
+  debugConsole.println(123456789123456789.123456789123456789); //should trigger overflow
   delay(5000);
 }
